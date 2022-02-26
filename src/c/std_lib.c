ef7a771ec69e76f9c425a646cff999b7f0876275
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-02-26 12:35:57 +0700

    fix strcpy

--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -7,13 +7,18 @@ int mod(int a, int n) {
     return a - n*(a/n);
 }
 
+void memcpy(byte *dest, byte *src, unsigned int n) {
+    // Mengcopy n bytes yang ditunjuk src ke dest
+}
+
+
 void strcpy(char *dst, char *src) {
     // Melakukan penyalinan null terminated string
     int i = 0;
-    while (src[i] != '\0') {
+    do {
         dst[i] = src[i];
         i ++;
-    } // src[i] == '\0'
+    } while (src[i] != '\0');// src[i] == '\0'
     dst[i] = '\0';
 }
 

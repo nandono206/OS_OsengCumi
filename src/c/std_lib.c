bd39079fb7d9fef7c4e4c5efc63b29fc6095f714
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-02-26 11:43:55 +0700

    add strcpy

--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -1,3 +1,14 @@
 #include "header/std_lib.h"
 
 // Opsional : Tambahkan implementasi kode C
+
+void strcpy(char *dst, char *src) {
+    // Melakukan penyalinan null terminated string
+    int i = 0;
+    while (src[i] != '\0') {
+        dst[i] = src[i];
+        i ++;
+    } // src[i] == '\0'
+    dst[i] = '\0';
+}
+

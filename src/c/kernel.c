e125dbd4088f0974e669e485757c9feda0f8bd45
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-04-04 00:11:25 +0700

    update handleInterrupt21

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -20,17 +20,30 @@ int main() {
 }
 
 void handleInterrupt21(int AX, int BX, int CX, int DX) {
-  switch (AX) {
-    case 0x0:
-      printString(BX);
-      break;
-    case 0x1:
-      readString(BX);
-      break;
-    default:
-      printString("Invalid interrupt");
-  }
+    switch (AX) {
+        case 0x0:
+            printString(BX);
+            break;
+        case 0x1:
+            readString(BX);
+            break;
+        case 0x2:
+            readSector(BX, CX);
+            break;
+        case 0x3:
+            writeSector(BX, CX);
+            break;
+        case 0x4:
+            read(BX, CX);
+            break;
+        case 0x5:
+            write(BX, CX);
+            break;
+        default:
+            printString("Invalid Interrupt");
+    }
 }
+
 
 void printString(char *string) {
   int i = 0;
